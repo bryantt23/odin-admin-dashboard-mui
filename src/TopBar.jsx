54457d39800c65a1eb059c99d9fecc415f27f811
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-03 05:08:25 -0700

    chat gpt feedback

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SearchIcon from "@mui/icons-material/Search";
-import { TextField, InputAdornment, Grid, Typography, Button } from '@mui/material';
+import { TextField, InputAdornment, Grid, Typography, Button, Avatar } from '@mui/material';
 import AlarmIcon from "@mui/icons-material/Alarm";
+import goldenImage from '../golden.png';
 
 function TopBar() {
     return (
         <Grid container>
-            <Grid xs={7} sx={{ backgroundColor: 'yellow', padding: "5px 15px 5px 15px" }}>
+            <Grid item xs={7} sx={{ backgroundColor: 'yellow', padding: "5px 15px 5px 15px" }}>
                 <TextField
                     variant='outlined'
                     fullWidth
@@ -18,22 +19,21 @@ function TopBar() {
                         )
                     }}></TextField>
             </Grid>
-            <Grid xs={5} sx={{ backgroundColor: '#90ee90', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Grid item xs={5} sx={{ backgroundColor: '#90ee90', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <AlarmIcon sx={{ margin: 2 }} />
-                <img src="../golden.png" style={{ height: 30, margin: 2, backgroundColor: 'white', borderRadius: '50%' }}></img>
+                <Avatar src={goldenImage} sx={{ width: 30, height: 30, margin: 2, backgroundColor: 'white', }}></Avatar>
                 <Typography sx={{ margin: 2 }}>Morgan Oakley</Typography>
             </Grid>
-            <Grid xs={6} sx={{ backgroundColor: '#90aa90', display: 'flex', alignItems: 'center', padding: 2 }}>
+            <Grid item xs={6} sx={{ backgroundColor: '#90aa90', display: 'flex', alignItems: 'center', padding: 2 }}>
                 <Grid item sx={{ paddingRight: 2 }}>
-                    <img src="../golden.png" style={{ height: 80, backgroundColor: 'white', borderRadius: '50%' }}></img>
+                    <Avatar src={goldenImage} sx={{ height: 80, width: 80, backgroundColor: 'white', }}></Avatar>
                 </Grid>
                 <Grid item>
                     <Typography >Hi there,</Typography>
                     <Typography variant="h5">Morgan Oakley</Typography>
                 </Grid>
-
             </Grid>
-            <Grid xs={6} sx={{ backgroundColor: '#90cc90', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: 5 }}>
+            <Grid item xs={6} sx={{ backgroundColor: '#90cc90', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: 5 }}>
                 <Button variant="contained" sx={{ borderRadius: 25 }}>New</Button>
                 <Button variant="contained" sx={{ borderRadius: 25 }}>Upload</Button>
                 <Button variant="contained" sx={{ borderRadius: 25 }}>Share</Button>
