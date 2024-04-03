393d415b7685ac9f317c5e0b16c2e16f488a6475
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-03 04:59:55 -0700

    bottom left top bar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SearchIcon from "@mui/icons-material/Search";
-import { TextField, InputAdornment, Grid, Typography } from '@mui/material';
+import { TextField, InputAdornment, Grid, Typography, Button } from '@mui/material';
 import AlarmIcon from "@mui/icons-material/Alarm";
 
 function TopBar() {
@@ -19,9 +19,24 @@ function TopBar() {
                     }}></TextField>
             </Grid>
             <Grid xs={5} sx={{ backgroundColor: '#90ee90', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <AlarmIcon sx={{ margin: 1 }} />
-                <img src="../golden.png" style={{ height: 30, margin: 1 }}></img>
-                <Typography sx={{ margin: 1 }}>Morgan Oakley</Typography>
+                <AlarmIcon sx={{ margin: 2 }} />
+                <img src="../golden.png" style={{ height: 30, margin: 2, backgroundColor: 'white', borderRadius: '50%' }}></img>
+                <Typography sx={{ margin: 2 }}>Morgan Oakley</Typography>
+            </Grid>
+            <Grid xs={6} sx={{ backgroundColor: '#90aa90', display: 'flex', alignItems: 'center', padding: 2 }}>
+                <Grid item sx={{ paddingRight: 2 }}>
+                    <img src="../golden.png" style={{ height: 80, backgroundColor: 'white', borderRadius: '50%' }}></img>
+                </Grid>
+                <Grid item>
+                    <Typography >Hi there,</Typography>
+                    <Typography variant="h5">Morgan Oakley</Typography>
+                </Grid>
+
+            </Grid>
+            <Grid xs={6} sx={{ backgroundColor: '#90cc90', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: 5 }}>
+                <Button variant="contained" sx={{ borderRadius: 25 }}>New</Button>
+                <Button variant="contained" sx={{ borderRadius: 25 }}>Upload</Button>
+                <Button variant="contained" sx={{ borderRadius: 25 }}>Share</Button>
             </Grid>
         </Grid>
     )
