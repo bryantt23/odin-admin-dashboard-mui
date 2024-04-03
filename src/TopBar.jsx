04ef4ba45bbd55791cfc55ae49ff9846942f439c
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-04-03 03:12:00 -0700

    top right top bar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SearchIcon from "@mui/icons-material/Search";
-import { TextField, InputAdornment, Grid } from '@mui/material';
+import { TextField, InputAdornment, Grid, Typography } from '@mui/material';
 import AlarmIcon from "@mui/icons-material/Alarm";
 
 function TopBar() {
@@ -18,7 +18,11 @@ function TopBar() {
                         )
                     }}></TextField>
             </Grid>
-            <Grid xs={5} sx={{ backgroundColor: '#90ee90' }}></Grid>
+            <Grid xs={5} sx={{ backgroundColor: '#90ee90', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <AlarmIcon sx={{ margin: 1 }} />
+                <img src="../golden.png" style={{ height: 30, margin: 1 }}></img>
+                <Typography sx={{ margin: 1 }}>Morgan Oakley</Typography>
+            </Grid>
         </Grid>
     )
 }
